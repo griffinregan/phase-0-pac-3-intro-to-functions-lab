bb0e64120b75e7a104f188211b4980bd27c83b68
griffincregan <griffinregan814@gmail.com>
2022-04-28 18:41:27 -0500

    commit tree 33bfeada32ca91ecf18880541a4d016c05224958

---
+++ index.js
@@ -0,0 +1,13 @@
+function shout(string) {
+    return string.toUpperCase();
+  } function whisper(string) { 
+      return string.toLowerCase();
+  } function logShout(string) {
+      return console.log.toUppercase();
+  } function logShout(string) {
+    console.log("HELLO");
+  }function logWhisper(string) {
+    console.log("hello");
+  } function sayHiToHeadphonedRoommate(string) {
+      console.log("hello" = "I can't hear you!" )
+  }
